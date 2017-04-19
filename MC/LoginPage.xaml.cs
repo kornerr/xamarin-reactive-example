c20a185398deed884aea4abe8d50dd4f4efc8e8b
Michael Kapelko <kapelko@magora-systems.com>
2017-04-19 15:19:02 +0700

    Make LoginPage use LoginVM

--- MC/LoginPage.xaml.cs
+++ MC/LoginPage.xaml.cs
@@ -1,15 +1,43 @@
-﻿using System;
+﻿using ReactiveUI;
+using System;
 using System.Collections.Generic;
-
 using Xamarin.Forms;
 
 namespace MC
 {
-	public partial class LoginPage : ContentPage
+	public partial class LoginPage : ContentPage, IViewFor<LoginVM>
 	{
 		public LoginPage()
 		{
 			InitializeComponent();
+
+            ViewModel = new LoginVM();
+
+            this.Bind(ViewModel, vm => vm.Username, v => v.Username.Text);
+            this.Bind(ViewModel, vm => vm.Password, v => v.Password.Text);
+            this.BindCommand(ViewModel, vm => vm.Login, v => v.Login);
 		}
+
+        // Boilerplate code.
+
+        public static readonly BindableProperty ViewModelProperty =
+            BindableProperty.Create(
+                nameof(ViewModel),
+                typeof(LoginVM),
+                typeof(LoginPage),
+                null,
+                BindingMode.OneWay);
+        public LoginVM ViewModel
+        {
+            get { return (LoginVM)GetValue(ViewModelProperty); }
+            set { SetValue(ViewModelProperty, value); }
+        }
+
+        object IViewFor.ViewModel
+        {
+            get { return ViewModel; }
+            set { ViewModel = (LoginVM)value; }
+        }
 	}
 }
+
