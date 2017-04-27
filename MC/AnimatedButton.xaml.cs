bd5bf4fe4c4e3e79c2b905c9dc76208f258b25aa
Michael Kapelko <kapelko@magora-systems.com>
2017-04-27 15:48:22 +0700

    Add bindable command property to AnimatedButton, but don't yet use it

--- MC/AnimatedButton.xaml.cs
+++ MC/AnimatedButton.xaml.cs
@@ -1,17 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace MC
 {
 	public partial class AnimatedButton : ContentView
 	{
+        public const string ButtonClickedCommandPropertyName = "ButtonClickedCommand";
+        public static BindableProperty ButtonClickedCommandProperty =
+            BindableProperty.Create(
+                "ButtonClickedCommand",
+                typeof(ICommand),
+                typeof(AnimatedButton),
+                null);
+
+        public ICommand ButtonClickedCommand
+        {
+            get { return (ICommand)GetValue(ButtonClickedCommandProperty); }
+            set { SetValue(ButtonClickedCommandProperty, value); }
+        }
+
 		public AnimatedButton()
 		{
 			InitializeComponent();
 
             setupTap();
 		}
+
+        protected override void OnBindingContextChanged()
+        {
+            base.OnBindingContextChanged();
+            RemoveBinding(ButtonClickedCommandProperty);
+            SetBinding(
+                ButtonClickedCommandProperty,
+                new Binding(ButtonClickedCommandPropertyName));
+        }
 
         private void setupTap()
         {
@@ -20,6 +45,13 @@ namespace MC
             {
                 await this.ScaleTo(0.95, 50, Easing.CubicOut);
                 await this.ScaleTo(1, 50, Easing.CubicIn);
+				Debug.WriteLine("AnimatedButton. Finished animating");
+                // Report.
+                var command = ButtonClickedCommand;
+                if (command != null) {
+					Debug.WriteLine("AnimatedButton. Executing command");
+					command.Execute(null);
+                }
             };
             this.GestureRecognizers.Add(tapRecognizer);
         }
