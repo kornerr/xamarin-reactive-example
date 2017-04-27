8df848a3dc6dd1febb5f1db89eae35e364540d30
Michael Kapelko <kapelko@magora-systems.com>
2017-04-27 14:07:30 +0700

    Add AnimatedButton stub

---
+++ MC/AnimatedButton.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace MC
+{
+	public partial class AnimatedButton : ContentView
+	{
+		public AnimatedButton()
+		{
+			InitializeComponent();
+		}
+	}
+}
